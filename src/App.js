b797e847d9043d391dc2a41f7956ed7df7f08931
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2020-02-14 18:56:14 -0600

    Main structure of time control component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Control from './Control';
+import Procesos from './Procesos';
+import CPU from './CPU';
+import Memoria from './Memoria';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Control />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
